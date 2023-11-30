0f4328f5ba8de951b4c6c5ec1ff6263f2802f297
nave <148296413+whosnave@users.noreply.github.com>
2023-11-30 19:31:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ for (let i = 1; i <= 100; i++) {
 
 let startGame = () => {
   alert(
-    "Selamat datang di MindMatrix!\n\nDimana kamu akan memilih angka dari 1 - 99 dan jika angka pilihan kamu lebih kecil maka kamu kalah."
+    "Selamat datang di MindMatrix!\n\nDimana Anda akan memilih angka dari 1 - 99 dan jika angka pilihan Anda lebih kecil maka Anda kalah."
   );
 
   let angkaUser = prompt("Masukkan angka pilihan Anda!");
   let randomize = Math.floor(Math.random() * angkaRandom.length);
 
   if (!isNaN(angkaUser) && angkaUser >= 1 && angkaUser <= 99) {
-    if (angkaUser > angkaRandom[randomize]) {
+    if (angkaUser > randomize) {
       document.write(
         `Selamat! Angka kamu lebih besar dari angka yang di acak! <br> <b>${angkaUser}</b> > ${randomize}`
       );
-    } else if (angkaUser < angkaRandom[randomize]) {
+    } else if (angkaUser < randomize) {
       document.write(
         `Maaf kamu gagal... <br> <b>${angkaUser}</b> < ${randomize}`
       );
-    } else {
+    } else if (angkaUser == randomize) {
       document.write(`Hasil seri! <br> ${angkaUser} = ${randomize}`);
     }
   } else {
     alert("Masukkan angka yang valid!");
   }
-}
+};
 
 startGame();
